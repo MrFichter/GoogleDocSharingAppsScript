a96ff1877422cfda3a217cf174348e478718cc1f
Jonathan Fichter <jonathan.fichter@live.com>
2012-09-22 10:49:57 -0400

    Changes to readme.txt.

--- GoogleDocSharingAppsScript.js
+++ GoogleDocSharingAppsScript.js
@@ -1,6 +1,7 @@
-/** Google-Doc-Sharing Apps Script
+/* Google-Doc-Sharing Apps Script
 
-by Jonathan Fichter
+by Jonathan Fichter, with special thanks to my brother and to the starter
+code that Google provides
 
 When connected to a Google spreadsheet under an account associated with
 Google Apps for Education, Business, or Government, this program will use 
@@ -12,11 +13,14 @@ documents you wish to create, and populate Column A of the spreadsheet with
 the email address of the desired editor for each document.
 
 To connect this script with the spreadsheet, click "Tools" from the spreadsheet
-menu. Then select "Script Editor" and "Blank Project." Then, paste this text 
-and everything below into the script editor and save it. I'm not sure if this is 
-necessary, but I usually select "Run" --> "onOpen" once before closing evertyhing.
-Then, the next time you open your spreadsheet, a new menu item will appear called
-"MrFichter's Doc-Sharing Script."
+menu. Then select "Script Editor" and "Blank Project." (Please make sure it's
+really blank. You may need to delete some starter code that Google creates 
+for you.) Then, paste in all of the text you'll find in the file above entitled
+GoogleDocSharingAppsScript. 
+
+I'm not sure if this is necessary, but I usually select "Run" --> "onOpen" once 
+before closing evertyhing. Then, the next time you open your spreadsheet, a new 
+menu item will appear called "MrFichter's Doc-Sharing Script."
 
 This menu item will enable you to run the script!
 
